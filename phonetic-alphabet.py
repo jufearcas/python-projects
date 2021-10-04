9c27372f0a89c012b64f5fbd33311edce7a9cfdb
Juan Fernando Araujo Castellanos <72634639+jufearcas@users.noreply.github.com>
2021-10-03 21:07:43 -0500

    Add files via upload

---
+++ phonetic-alphabet.py
@@ -0,0 +1,76 @@
+print("Phonetic alphabet")
+
+letter= input("Enter the letter: ")
+
+#letra = str(letter)
+
+
+if letter == "a" or letter == "A":
+    print("Alfa")
+
+elif letter == "b" or letter == "B":
+    print("Bravo")
+
+#print( "elif letter == \"\" or \"\"\:\print\(\"\"\) \n" * 22)
+#code to print and copy past 
+elif letter == "C" or letter == "c":
+    print("Charlie")
+elif letter == "d" or letter == "D":
+    print("Delta")
+
+elif letter == "E" or letter == "e":
+    print("As in Echo")
+elif letter == "F" or letter == "f":
+    print("As in Foxtrot")
+elif letter == "G" or letter == "g":
+    print("As in Golf")
+elif letter == "h" or  letter == "H":
+    print("As in Hotel")
+elif letter == "I" or letter == "i":
+    print("As in India")
+elif letter == "J" or letter == "j":
+    print("As in Juliett")
+    
+elif letter == "K" or letter == "k":
+    print("As in Kilo")
+
+elif letter == "L" or letter == "l":
+    print("As in Lima")
+
+elif letter == "m" or letter == "M":
+    print("Mike")
+
+elif letter == "N" or letter == "n":
+    print("November")
+
+elif letter == "O" or letter == "o":
+    print("Oscar")
+
+elif letter == "P" or letter == "p":
+    print("As in Papa")
+elif letter == "Q" or letter == "q":
+    print("As in Quebec")
+
+elif letter == "r" or letter == "R":
+    print("As in Romeo")
+elif letter == "s" or letter == "S":
+    print("As in Sierra")
+elif letter == "t" or letter == "T":
+    print("As in Tango")
+elif letter == "u" or letter == "U":
+    print("As in Uniform")
+elif letter == "V" or letter =="v":
+    print("As in Victor")
+elif letter == "W" or letter == "w":
+    print("As in Whiskey")
+elif letter == "x" or letter == "X":
+    print("As in X-Ray")
+
+elif letter == "Y" or letter == "y":
+    print("As in Yankee")
+
+elif letter == "z" or letter == "Z":
+    print("As in Zulu")
+
+else: 
+    print("Not a letter")
